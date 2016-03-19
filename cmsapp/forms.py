7b4d9cec1b7f60f4325e0c8863fd087ba24af376
koallen <weifengzi2009@gmail.com>
2016-03-19 20:36:21 +0800

    change form to modelForm

--- cmsapp/forms.py
+++ cmsapp/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 
-class LoginForm(forms.Form):
+class LoginForm(forms.ModelForm):
     """
     Form to log the user in
     """
 
-    #class Meta:
-    #    model = User
-    #    fields = ["username", "password"]
-    username = forms.CharField()
-    password = forms.CharField()
+    class Meta:
+        model = User
+        fields = ["username", "password"]
