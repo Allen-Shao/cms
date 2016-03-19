3cf3ece8dee38bf4e9265eff22294f872e248b68
koallen <weifengzi2009@gmail.com>
2016-03-19 21:35:23 +0800

    register models in admin

--- cmsapp/admin.py
+++ cmsapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import CallCenterReport, Decision, Crisis, Notification, Place, Agency, UsefulPlace, ResourceRequest
 
 # Register your models here.
+admin.site.register(CallCenterReport)
+admin.site.register(Decision)
+admin.site.register(Crisis)
+admin.site.register(Notification)
+admin.site.register(Place)
+admin.site.register(Agency)
+admin.site.register(UsefulPlace)
+admin.site.register(ResourceRequest)
