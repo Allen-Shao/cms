fb9a7d1e38f2da1388cac7b84ac3229156c7fa34
koallen <weifengzi2009@gmail.com>
2016-03-19 16:59:54 +0800

    add URL definition, add view classes

--- cmsapp/views.py
+++ cmsapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+class HomeView():
+
+    pass
+
+class DashboardView():
+
+    pass
+
+class NotificationView():
+
+    pass
+
+class ReportView():
+
+    pass
+
+class ResourceView():
+
+    pass
+
+class LoginView():
+
+    pass
