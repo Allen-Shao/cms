2e31438dd6408977451bea431947e1e08226cc6c
iamqingmei <iamqingmei@gmail.com>
2016-03-21 01:16:38 +0800

    about active

--- cmsapp/views.py
+++ cmsapp/views.py
@@ -139,3 +139,7 @@ class LogoutView(RedirectView):
 class AboutView(TemplateView):
 
     template_name = "about.html"
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context["about_active"] = "active"
+        return context
