55d90b8d8a99277e939f7fd0e9c76bff63968246
koallen <weifengzi2009@gmail.com>
2016-03-23 01:07:42 +0800

    change field display

--- cmsapi/serializers.py
+++ cmsapi/serializers.py
@@ -6,6 +6,16 @@ class ReportSerializer(serializers.ModelSerializer):
     Serializer for call center reports
     """
 
+    # set custom display method
+    type_of_assistance = serializers.SerializerMethodField()
+    type_of_crisis = serializers.SlugRelatedField(
+        read_only = True,
+        slug_field = "type_of_crisis"
+    )
+
     class Meta:
         model = CallCenterReport
         exclude = ["status"]
+
+    def get_type_of_assistance(self, report):
+        return report.get_type_of_assistance_display()
