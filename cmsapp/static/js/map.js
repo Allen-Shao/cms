4b8cd4e07d134e3067c22b2aaf2254e38a3f248f
koallen <weifengzi2009@gmail.com>
2016-03-23 00:25:39 +0800

    add markers

--- cmsapp/static/js/map.js
+++ cmsapp/static/js/map.js
@@ -5,11 +5,55 @@ function initMap() {
     center: {lat: 1.354241, lng: 103.819417}
   });
 
+  // psi icon
+  var psiIcon = {
+    // url: "http://cdn8.staztic.com/app/a/2742/2742678/psi-malaysia-haze-5-l-140x140.png",
+    url: "http://10.27.161.249:8888/static/images/psi_marker_40.png",
+    size: new google.maps.Size(40, 40)
+  };
+
   // create marker
-  var marker = new google.maps.Marker({
-    position: {lat: 1.354241, lng: 103.819417},
+  // var marker = new MarkerWithLabel({
+  //   position: {lat: 1.354241, lng: 103.819417},
+  //   map: map,
+  //   draggable: false,
+  //   labelContent: "59",
+  //   icon: psiIcon
+  // });
+
+  var rNO = new google.maps.Marker({
+    position: {lat: 1.41803, lng: 103.82000},
     map: map,
-    title: 'Hello World!'
+    title: 'Hello World!',
+    icon: psiIcon
+  });
+
+  var rCE = new google.maps.Marker({
+    position: {lat: 1.35735, lng: 103.82000},
+    map: map,
+    title: 'Hello World!',
+    icon: psiIcon
+  });
+
+  var rEA = new google.maps.Marker({
+    position: {lat: 1.35735, lng: 103.94000},
+    map: map,
+    title: 'Hello World!',
+    icon: psiIcon
+  });
+
+  var rWE = new google.maps.Marker({
+    position: {lat: 1.35735, lng: 103.70000},
+    map: map,
+    title: 'Hello World!',
+    icon: psiIcon
+  });
+
+  var rSO = new google.maps.Marker({
+    position: {lat: 1.29587, lng: 103.82000},
+    map: map,
+    title: 'Hello World!',
+    icon: psiIcon
   });
 
   // create dengue cluster
