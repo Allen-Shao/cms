fa64da4872daffea3231bd822fa4aa9982846796
iamqingmei <iamqingmei@gmail.com>
2016-03-23 22:58:01 +0800

    fade 2 seconds

--- cmsapp/static/js/Timeout.js
+++ cmsapp/static/js/Timeout.js
@@ -1,8 +1,4 @@
 setTimeout(function() {
-    $('#mydiv').fadeOut('fast');
-}, 3000); // <-- time in milliseconds
-
-setTimeout(function() {
     $('#fastfade').fadeOut('fast');
 }, 2000); // <-- time in milliseconds
 
