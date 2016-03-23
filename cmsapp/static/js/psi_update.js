0aac5542cdeeb1c8febe17ff1b7aefe23b675744
koallen <weifengzi2009@gmail.com>
2016-03-23 19:45:55 +0800

    show psi in home.html

--- cmsapp/static/js/psi_update.js
+++ cmsapp/static/js/psi_update.js
@@ -1,25 +1,37 @@
- updatePSI();
+updatePSI();
 
- function updatePSI() {
+/**
+  * update psi readings
+  */
+function updatePSI() {
   $.ajax({
     type: "GET",
     dataType: "xml",
     url: "http://www.nea.gov.sg/api/WebAPI?dataset=psi_update&keyref=781CF461BB6606AD0308169EFFAA8231021BA33828C73DAE",
-    success: function(data){
-      console.log(data.getElementsByTagName("region")[1].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value')); // Overall PSI
-
-      console.log(data.getElementsByTagName("region")[0].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value')); // North
-
-      console.log(data.getElementsByTagName("region")[2].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value')); // Central
-
-      console.log(data.getElementsByTagName("region")[3].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value')); // East
-
-      console.log(data.getElementsByTagName("region")[4].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value')); // West
-
-      console.log(data.getElementsByTagName("region")[5].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value')); // South
-    },
+    success: updateInHome,
     error: function(error) {
       console.log(error);
     }
   });
+}
+
+function updateInHome(psiData) {
+  // get reading from XML
+  var overallReading = psiData.getElementsByTagName("region")[1].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value');
+  var northReading = psiData.getElementsByTagName("region")[0].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value');
+  var centralReading = psiData.getElementsByTagName("region")[2].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value');
+  var eastReading = psiData.getElementsByTagName("region")[3].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value');
+  var westReading = psiData.getElementsByTagName("region")[4].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value');
+  var southReading = psiData.getElementsByTagName("region")[5].getElementsByTagName("record")[0].getElementsByTagName("reading")[0].getAttribute('value');
+
+  // compose html tags
+  var psiReading = "<p>Overall: " + overallReading + "</p>";
+  psiReading += "<p>North: " + northReading + "</p>";
+  psiReading += "<p>Central: " + centralReading + "</p>";
+  psiReading += "<p>East: " + eastReading + "</p>";
+  psiReading += "<p>West: " + westReading + "</p>";
+  psiReading += "<p>South: " + southReading + "</p>";
+
+  // reflect change in html
+  $("#weather_psi").html(psiReading);
 }
