de18e1aa17c4856eee6952e4c6d90912788c4038
koallen <weifengzi2009@gmail.com>
2016-03-29 23:34:26 +0800

    fix refresh - -

--- cmsapp/static/js/report_update.js
+++ cmsapp/static/js/report_update.js
@@ -10,23 +10,23 @@ function pullReports() {
     url: currentURL,
     success: function(data){
       var html = '';
-        for (var x = 0; x < data.results.length; x++) {
-          html += "<tr id='row" + data.results[x].id + "'>";
-          html += "<th class='col-md-2'>";
-          html += data.results[x].name;
-          html += "</th><th class='col-md-2'>";
-          html += data.results[x].location;
-          html += "</th><th class='col-md-3'>";
-          html += data.results[x].description;
-          html += "</th><th class='col-md-3'>";
-          html += data.results[x].type_of_assistance;
-          html += "</th><th class='col-md-2'>";
-          html += data.results[x].type_of_crisis;
-          html += "</th><th class='col-md-1'><button style='width:100%;' type='button' id='" + data.results[x].id + "' class='btn btn-xs btn-success' onClick='updateReportStatus(this.id, true)'><span class='glyphicon glyphicon-ok'></span>&nbsp;</button>"
-          html += "</th><th class='col-md-1'><button style='width:100%;' type='button' id='" + data.results[x].id + "' class='btn btn-xs btn-danger' onClick='updateReportStatus(this.id, false)'><span class='glyphicon glyphicon-remove'></span>&nbsp;</button>"
-          html += "</th></tr>";
-        }
-        $("#ReportContent").html(html);
+      for (var x = 0; x < data.results.length; x++) {
+        html += "<tr id='row" + data.results[x].id + "'>";
+        html += "<th class='col-md-2'>";
+        html += data.results[x].name;
+        html += "</th><th class='col-md-2'>";
+        html += data.results[x].location;
+        html += "</th><th class='col-md-3'>";
+        html += data.results[x].description;
+        html += "</th><th class='col-md-3'>";
+        html += data.results[x].type_of_assistance;
+        html += "</th><th class='col-md-2'>";
+        html += data.results[x].type_of_crisis;
+        html += "</th><th class='col-md-1'><button style='width:100%;' type='button' id='" + data.results[x].id + "' class='btn btn-xs btn-success' onClick='updateReportStatus(this.id, true)'><span class='glyphicon glyphicon-ok'></span>&nbsp;</button>"
+        html += "</th><th class='col-md-1'><button style='width:100%;' type='button' id='" + data.results[x].id + "' class='btn btn-xs btn-danger' onClick='updateReportStatus(this.id, false)'><span class='glyphicon glyphicon-remove'></span>&nbsp;</button>"
+        html += "</th></tr>";
+      }
+      $("#ReportContent").html(html);
     }
   });
 }
