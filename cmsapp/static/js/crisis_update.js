e1eadeccafa3a9f44621dec596d9ddbe064b0fcb
iamqingmei <iamqingmei@gmail.com>
2016-03-29 23:36:23 +0800

    fix crisis-update and remove google map in process-reports

--- cmsapp/static/js/crisis_update.js
+++ cmsapp/static/js/crisis_update.js
@@ -1,5 +1,4 @@
-var crisis_old_l = 0;
-var crisis_new_l = 0;
+
 pullDecisions();
 
 setInterval(pullDecisions, 5000);
@@ -11,10 +10,9 @@ function pullDecisions() {
    dataType: "json",
    url: "http://localhost:8888/api/decisions/",
    success: function(data){
-    crisis_new_l = data.results.length;
+    length = data.results.length;
 
-    if (crisis_new_l != crisis_old_l) {
-      for (var x = crisis_old_l; x < crisis_new_l; x++) {
+      for (var x = 0; x < length; x++) {
         html = "";
           // if (data[x].active){
             html += "<tr>";
@@ -26,12 +24,9 @@ function pullDecisions() {
             html += data.results[x].date_time;
             html += "</th></tr>";
 
-            $("#DecisionsContent").append(html);
+            $("#DecisionsContent").html(html);
           // }
         }
-
-        crisis_old_l = crisis_new_l;
-      }
     }
   });
 }
