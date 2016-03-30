e9071e39461134e63431e359f49ab5cdebdb0bf9
koallen <weifengzi2009@gmail.com>
2016-03-30 20:28:09 +0800

    support decision deactivation ⚆ _ ⚆

--- cmsapp/static/js/crisis_update.js
+++ cmsapp/static/js/crisis_update.js
@@ -16,14 +16,33 @@ function pullDecisions() {
         html += "<tr>";
         html += "<th class='col-md-2'>";
         html += data.results[x].type_of_crisis;
-        html += "</th><th class='col-md-2'>";
+        html += "</th><th class='col-md-6'>";
         html += data.results[x].description;
         html += "</th><th class='col-md-3'>";
         html += data.results[x].date_time;
+        html += "</th><th class='col-md-1'>";
+        html += "<button style='width:100%;' type='button' id='" + data.results[x].id + "' class='btn btn-xs btn-danger' onClick='updateDecisionStatus(this.id, false)'><span class='glyphicon glyphicon-remove'></span>&nbsp;</button>"
         html += "</th></tr>";
       }
-      
+
       $("#DecisionsContent").html(html);
+    }
+  });
+}
+
+function updateDecisionStatus(decisionID, newStatus) {
+  var csrf_token = getCookie("csrftoken");
+  var dataToPost = {"active": newStatus}
+  $.ajax({
+    url: "http://localhost:8888/api/decisions/" + decisionID + "/",
+    headers: {"X-CSRFToken": csrf_token},
+    data: JSON.stringify(dataToPost),
+    method: "PATCH",
+    contentType: 'application/json',
+    dataType: "json",
+    success: function(data) {
+      pullDecisions();
+      console.log("Decision status updated");
     }
   });
 }
