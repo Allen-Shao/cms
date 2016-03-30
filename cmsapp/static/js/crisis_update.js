cae98f18224939f0474c7516b79ccca0e51e5d44
koallen <weifengzi2009@gmail.com>
2016-03-30 14:36:07 +0800

    fix crisis_update

--- cmsapp/static/js/crisis_update.js
+++ cmsapp/static/js/crisis_update.js
@@ -1,4 +1,3 @@
-
 pullDecisions();
 
 setInterval(pullDecisions, 5000);
@@ -6,28 +5,28 @@ setInterval(pullDecisions, 5000);
 function pullDecisions() {
   var html = '';
   $.ajax({
-   type: "GET",
-   dataType: "json",
-   url: "http://localhost:8888/api/decisions/",
-   success: function(data){
-    length = data.results.length;
-
+    type: "GET",
+    dataType: "json",
+    url: "http://localhost:8888/api/decisions/",
+    success: function(data){
+      length = data.results.length;
+      html = "";
+      
       for (var x = 0; x < length; x++) {
-        html = "";
-          // if (data[x].active){
-            html += "<tr>";
-            html += "<th class='col-md-2'>";
-            html += data.results[x].type_of_crisis;
-            html += "</th><th class='col-md-2'>";
-            html += data.results[x].description;
-            html += "</th><th class='col-md-3'>";
-            html += data.results[x].date_time;
-            html += "</th></tr>";
+        // if (data[x].active){
+        html += "<tr>";
+        html += "<th class='col-md-2'>";
+        html += data.results[x].type_of_crisis;
+        html += "</th><th class='col-md-2'>";
+        html += data.results[x].description;
+        html += "</th><th class='col-md-3'>";
+        html += data.results[x].date_time;
+        html += "</th></tr>";
 
-            
-          // }
-        }
-        $("#DecisionsContent").html(html);
+
+        // }
+      }
+      $("#DecisionsContent").html(html);
     }
   });
 }
