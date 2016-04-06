80296c97f8ed5135bd9c0ad477c284e57f28b543
koallen <weifengzi2009@gmail.com>
2016-04-06 21:03:06 +0800

    add testing coverage

--- cmsapp/models.py
+++ cmsapp/models.py
@@ -59,6 +59,7 @@ class Notification(models.Model):
     place = models.ForeignKey("Place")
 
     def __unicode__(self):
+        #return "sss"
         return "%s - %s" % (self.decision, self.place)
 
 class Place(models.Model):
--- cmsapp/tests.py
+++ cmsapp/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from models import CallCenterReport, Crisis
 
 class HomeViewTestCase(TestCase):
     def test_home_page(self):
@@ -8,3 +9,16 @@ class HomeViewTestCase(TestCase):
     def test_login_page(self):
         response = self.client.get("/login/")
         self.assertEqual(response.status_code, 200)
+
+class ModelTestCase(TestCase):
+    def test_call_center_report(self):
+        report = CallCenterReport.objects.create(
+            name="Test",
+            mobile_number="99999999",
+            location="singapore",
+            description="help me",
+            type_of_crisis=Crisis.objects.create(type_of_crisis="Haze"),
+            type_of_assistance="EA",
+        )
+        self.assertTrue(isinstance(report, CallCenterReport))
+        self.assertEqual(report.__unicode__(), "%s - %s" % (report.id, report.name))
