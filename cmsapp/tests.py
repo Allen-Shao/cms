7ccafc7c05b55c7375666500dffbaa582a562ea0
koallen <weifengzi2009@gmail.com>
2016-04-06 19:48:29 +0800

    add some sample tests

--- cmsapp/tests.py
+++ cmsapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomeViewTestCase(TestCase):
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        response = self.client.get("/login/")
+        self.assertEqual(response.status_code, 200)
